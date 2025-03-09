63a056c6b4b6ed40dd9ec38d0a280e2e5ab111f6
Kang-min Kim <rkdals0203@gmail.com>
2025-03-09 17:45:37 +0900

    inOrder

--- src/main/java/tree/inOrder/Solution.java
+++ src/main/java/tree/inOrder/Solution.java
@@ -18,6 +18,11 @@ class Node {
 class Solution {
 
     public static void inOrder(Node root) {
+        if(root!=null){
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
 
     }
 
