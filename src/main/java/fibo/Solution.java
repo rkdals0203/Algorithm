8dfd0971104e6b2e7cdce09ef59d415da09f7ba2
Kang-min Kim <rkdals0203@gmail.com>
2025-03-09 17:37:37 +0900

    isFibo

---
+++ src/main/java/fibo/Solution.java
@@ -0,0 +1,53 @@
+package main.java.fibo;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static String isFibo(long n) {
+        long n1 = 0;
+        long n2 = 1;
+        long sum = 0;
+        while(sum <= n){
+            if (sum == n) return "IsFibo";
+            sum = n1+n2;
+            n1 = n2;
+            n2 = sum;
+        }
+        return "IsNotFibo";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                String result = Result.isFibo(n);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
